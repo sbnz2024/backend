3d19b7edb3ef3817f01861b9715065582f021c02
seli0001 <nikolasehovac@gmail.com>
2024-05-07 07:00:49 +0200

    commit

--- app/run.sh
+++
@@ -1,3 +0,0 @@
-mvn clean package
-cd target
-java -jar app-1.0-SNAPSHOT.jar
--- app/src/main/java/demo/repository/RatingRepository.java
+++
@@ -1,8 +0,0 @@
-package demo.repository;
-
-import demo.model.Arrangement;
-import demo.model.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Integer> {
-}
